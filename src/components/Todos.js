cc7d95324ba8764df33901bcd2b377562d944aae
Pratima <62754762+Pratima0101@users.noreply.github.com>
2021-06-20 12:37:47 +0530

    commit

---
+++ src/components/Todos.js
@@ -0,0 +1,132 @@
+import React from "react";
+import { atom, useRecoilValue, useSetRecoilState } from "recoil";
+import { uid } from "react-uid";
+import { Button, InputLabel, FormControl, Input, TextField, Checkbox} from "@material-ui/core";
+import DeleteIcon from '@material-ui/icons/Delete';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import DoneAllIcon from '@material-ui/icons/DoneAll';
+const todoListState = atom({
+  key: "todoListState",
+  default: [],
+});
+
+
+
+export default function TodoList() {
+  const todoList = useRecoilValue(todoListState);
+  const setTodoList = useSetRecoilState(todoListState);
+
+  const handleComplete = (id) => {
+    setTodoList((oldTodoList) => {
+      return oldTodoList.map((item) => {
+        if (item.id === id) {
+          return {
+            ...item,
+            isCompleted: true
+          };
+        }
+
+        return item;
+      });
+    });
+  };
+
+  const handleDelete = (id) => {
+    setTodoList((oldTodoList) => {
+      return oldTodoList.filter((item) => item.id !== id);
+    });
+  };
+
+  return (
+    <>
+      <TodoForm />
+      <ul>
+        {todoList.map((item) => {
+          return (
+            <li key={item.id}>
+              <span
+                style={{
+                  textDecoration: item.isCompleted ? "line-through" : "unset"
+                }}
+              >
+                {item.text}{" "}
+              </span>
+              {!item.isCompleted ? (
+                  <button onClick={() => handleComplete(item.id)} className="btn" > <DoneAllIcon></DoneAllIcon> </button> 
+                
+      
+              ) : null}
+              <button onClick={() => handleDelete(item.id)} className="btn"> <DeleteIcon></DeleteIcon> </button> 
+              {/* <Button onClick={() => handleDelete(item.id)} variant="contained" color="secondary">
+               DELETE
+                </Button> */}
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+}
+
+
+// const currentDateTime =  Date.now();
+//   console.log(new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'}).format
+//   (currentDateTime));
+function TodoForm() {
+  const [inputValue, setInputValue] = React.useState("");
+  const setTodoList = useSetRecoilState(todoListState);
+  
+  const addItem = () => {
+
+    let timestamp = new Date();
+    let date = new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'}).format(timestamp)
+  
+    setTodoList((oldTodoList) => [
+      ...oldTodoList,
+      {
+        id: uid(`${inputValue}-${oldTodoList.length}`),
+        text: inputValue + ",  (" + date  +")",
+        isCompleted: false
+      }
+    ]);
+    // console.log(new Date());
+    return setInputValue("");
+  };
+
+  const handleKeypress = e => {
+    //it triggers by pressing the enter key
+    if (e.keyCode === 13) {
+      addItem();
+    }
+};
+ 
+
+  return (
+    <>
+
+         <label htmlFor="todo-field">New Todo</label>
+      {/* <input
+        id="todo-field"
+        type="text"
+        placeholder="write a todo"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      /> */}
+
+      
+
+        <input id="todo-field"
+        type="text"
+        placeholder="write a todo"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        onKeyPress={handleKeypress} />
+              
+      <button onClick={addItem} className="btn">
+      <AddCircleIcon></AddCircleIcon>
+            </button>
+      {/* <button onClick={addItem}>Add Todo</button> */}
+      
+    </>
+  );
+}
